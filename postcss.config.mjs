946298304159eab95cdc53107192c1a79f386a58
Antonia Mugisa <antoniamugisa@gmail.com>
2025-10-31 23:13:40 +0200

    addressed problems in code

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    "postcss-custom-properties": {},
+    "postcss-nesting": {}
+  },
 };
 
 export default config;
